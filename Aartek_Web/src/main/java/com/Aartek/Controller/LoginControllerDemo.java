207b71855828e61d0f1ee1cf5dc6f97facc15d53
maneeshchaurasiya@gmail.com <13Mca@#1042>
2018-07-18 10:26:27 +0530

    added code

--- Aartek_Web/src/main/java/com/Aartek/Controller/LoginControllerDemo.java
+++ Aartek_Web/src/main/java/com/Aartek/Controller/LoginControllerDemo.java
@@ -6,6 +6,9 @@ public class LoginControllerDemo {
 	System.out.println("Login Demo");
 	System.out.println("Simple changes");// Rohit verma
 	System.out.println("advance changes");
+	System.out.println("conflict  1111");
+	System.out.println("conflict  22222");
+	System.out.println("conflict  3333");
 	System.out.println("final changes");//Manish Chaurasiya
 	System.out.println("final");//Manish Chaurasiya
 	System.out.println("Simple2 changes");
